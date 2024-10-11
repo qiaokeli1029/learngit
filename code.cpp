051fe8e9f25bdbc4cb9bb9f650081e92032dafa2
ljl <3647044768@qq.com>
2024-10-11 21:58:22 +0800

    AND simple

--- code.cpp
+++ code.cpp
@@ -4,5 +4,5 @@ using namespace std;
 int main(){
     printf("Git is a distributed version control system\n");
     printf("Git is free software distributed under the GPL");
-    printf("Creating a new branch is quick");
+    printf("Creating a new branch is quick AND simple");
 }
