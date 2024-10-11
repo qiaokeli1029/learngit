efc2b4f0d9c0b222b625cdf831071c388457f44f
ljl <3647044768@qq.com>
2024-10-11 21:41:28 +0800

    branch test

--- code.cpp
+++ code.cpp
@@ -4,4 +4,5 @@ using namespace std;
 int main(){
     printf("Git is a distributed version control system\n");
     printf("Git is free software distributed under the GPL");
+    printf("Creating a new branch is quick");
 }
