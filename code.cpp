57ca95ea0cc52237f4f91f89fa7a8fd4dbc4d102
ljl <3647044768@qq.com>
2024-10-11 18:19:15 +0800

    append GPL

--- code.cpp
+++ code.cpp
@@ -3,5 +3,5 @@ using namespace std;
 
 int main(){
     printf("Git is a distributed version control system\n");
-    printf("Git is free software");
+    printf("Git is free software distributed under the GPL");
 }
