86deccd5993dd18544ed82b1535efc1062d0ed0c
ljl <3647044768@qq.com>
2024-10-12 09:49:03 +0800

    change code.cpp

--- code.cpp
+++ code.cpp
@@ -5,4 +5,5 @@ int main(){
     printf("Git is a distributed version control system\n");
     printf("Git is free software distributed under the GPL");
     printf("Creating a new branch is quick and simple");
+    printf("change");
 }
